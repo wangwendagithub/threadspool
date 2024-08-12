fd4568feadccbf3b2f894ebbe9ca249e9484320d
wwdcompany <1095596725@qq.com>
2024-08-12 11:30:48 +0800

    创建了main文件

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void sort(int arr[],int size)
+{
+	
+	
+}
+int main()
+{
+	int arr[] = {3,5,1,7,9,2};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	sort(arr,size);
+	rerurn 0;
+} 
